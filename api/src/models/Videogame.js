0f66427e166c8331bc55c12a00c1e016ba0308de
Diego Zamora <diegoz.estudio@gmail.com>
2022-02-26 19:35:50 -0300

    DB done, 100vg done

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,44 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "videogame",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      released: {
+        type: DataTypes.STRING,
+      },
+      rating: {
+        type: DataTypes.FLOAT,
+      },
+      platforms: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+      createdInDB: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,42 @@
-const { Router } = require('express');
+require("dotenv").config();
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
+const { API_KEY } = process.env;
+const axios = require("axios");
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+// [ ] GET /videogames:
+// Obtener un listado de los videojuegos
+// Debe devolver solo los datos necesarios para la ruta principal
+
+const getApiInfo = async () => {
+  try {
+    let i = 1;
+    let allGames = [];
+    while (i <= 5) {
+      let currentUrl = await axios.get(
+        `https://api.rawg.io/api/games?key=${API_KEY}&page=${i}`
+      );
+      let apiInfo = await currentUrl.data.results.map((e) => {
+        return {
+          id: e.id,
+          name: e.name,
+          // description: e.description,
+        };
+      });
+      allGames = [...allGames, ...apiInfo];
+      i++;
+    }
+    console.log(allGames);
+    console.log(allGames.length);
+  } catch (err) {
+    console.log("entré al catch :(", err);
+  }
+};
+getApiInfo();
 
 module.exports = router;
