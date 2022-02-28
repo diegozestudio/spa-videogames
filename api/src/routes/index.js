06fe3c91d2ee76747803f3575fb0cfdf7540202c
Diego Zamora <diegoz.estudio@gmail.com>
2022-02-28 16:46:27 -0300

    back done

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,14 +4,10 @@ const { Router } = require("express");
 // Ejemplo: const authRouter = require('./auth.js');
 const { API_KEY } = process.env;
 const axios = require("axios");
+const { Videogame, Genre } = require("../db");
 const router = Router();
-
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
-// [ ] GET /videogames:
-// Obtener un listado de los videojuegos
-// Debe devolver solo los datos necesarios para la ruta principal
 
 const getApiInfo = async () => {
   try {
@@ -21,22 +17,149 @@ const getApiInfo = async () => {
       let currentUrl = await axios.get(
         `https://api.rawg.io/api/games?key=${API_KEY}&page=${i}`
       );
-      let apiInfo = await currentUrl.data.results.map((e) => {
+      let apiInfo = currentUrl.data.results.map((e) => {
         return {
           id: e.id,
           name: e.name,
-          // description: e.description,
+          image: e.background_image,
+          genres: e.genres.map((g) => g.name),
         };
       });
       allGames = [...allGames, ...apiInfo];
       i++;
     }
-    console.log(allGames);
-    console.log(allGames.length);
+    // console.log(allGames);
+    // console.log(allGames.length);
+    return allGames;
   } catch (err) {
     console.log("entré al catch :(", err);
   }
 };
-getApiInfo();
+
+const getDbInfo = async () => {
+  return await Videogame.findAll({
+    include: {
+      model: Genre,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
+};
+
+const getAllGames = async () => {
+  try {
+    const apiInfo = await getApiInfo();
+    const dbInfo = await getDbInfo();
+    // const infoTotal = apiInfo.concat(dbInfo);
+    const infoTotal = [...apiInfo, ...dbInfo];
+    // console.log(infoTotal);
+    // console.log(infoTotal.length);
+    return infoTotal;
+  } catch (err) {
+    console.log("entré al catch :(", err);
+  }
+};
+
+router.get("/videogames", async (req, res) => {
+  try {
+    const name = req.query.name;
+    let charactersTotal = await getAllGames();
+    if (name) {
+      let characterName = await charactersTotal.filter((c) =>
+        c.name.toLowerCase().includes(name.toLowerCase())
+      );
+      characterName.length
+        ? res.status(200).send(characterName)
+        : res.status(400).send("No existe el videogame :(");
+    } else {
+      res.status(200).send(charactersTotal);
+    }
+  } catch (err) {
+    console.log("entré al catch :(", err);
+  }
+});
+
+router.get("/genres", async (req, res) => {
+  try {
+    const genresApi = await getApiInfo();
+    const genres = genresApi.map((c) => c.genres);
+    // console.log([...new Set(genres.flat())]);
+    const genresEach = [...new Set(genres.flat())];
+    genresEach.forEach((genre) => {
+      Genre.findOrCreate({
+        where: { name: genre },
+      });
+    });
+    const allGenres = await Genre.findAll();
+    res.send(allGenres);
+  } catch (err) {
+    console.log("entré al catch :(", err);
+  }
+});
+
+router.post("/videogame", async (req, res) => {
+  let {
+    name,
+    description,
+    released,
+    rating,
+    platforms,
+    image,
+    createdInDB,
+    genres,
+  } = req.body;
+  let newVideogame = await Videogame.create({
+    name,
+    description,
+    released,
+    rating,
+    platforms,
+    image,
+    createdInDB,
+  });
+  let genreDb = await Genre.findAll({
+    where: { name: genres },
+  });
+  newVideogame.addGenre(genreDb);
+  res.send("Videogame creado con exito");
+});
+
+router.get("/videogame/:id", async (req, res) => {
+  const { id } = req.params;
+  const gamesApi = await getApiInfo();
+  const gamesDB = await getDbInfo();
+  const findInApi = gamesApi.find((game) => game.id === Number(id));
+  const findInDB = gamesDB.find((game) => String(game.id) === id);
+  if (findInDB) {
+    res.send(findInDB);
+  } else if (findInApi) {
+    const gameFind = await axios.get(
+      `https://api.rawg.io/api/games/${id}?key=${API_KEY}`
+    );
+    let {
+      name,
+      genres,
+      background_image,
+      description,
+      released,
+      rating,
+      platforms,
+    } = gameFind.data;
+    const game = {
+      name,
+      genres: genres.map((e) => e.name),
+      image: background_image,
+      description,
+      released,
+      rating,
+      platforms: platforms.map((e) => e.platform.name),
+    };
+    res.send(game);
+  } else {
+    res.status(404).send("No existe el videogame");
+  }
+});
 
 module.exports = router;
